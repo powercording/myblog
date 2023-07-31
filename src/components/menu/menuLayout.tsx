fb99fd2b3c4194411dab0cf8ff9350406fbf0d62
powercording <adoim@naver.com>
2023-07-31 14:29:20 +0900

    Feat: 반응형 메뉴

--- src/components/menu/menuLayout.tsx
+++ src/components/menu/menuLayout.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import * as Nav from '@radix-ui/react-navigation-menu';
+import { useRef, useState } from 'react';
 import MenuItem, { MenuListProps } from './menuItem';
+import { SlOptionsVertical } from 'react-icons/sl';
 import { Session } from 'next-auth';
 
 interface MenuComponentProps {
@@ -10,14 +12,32 @@ interface MenuComponentProps {
 }
 
 export default function MenuLayout({ menuList, session }: MenuComponentProps) {
-  console.log();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLUListElement>(null);
   return (
-    <Nav.Root className="fixed top-0 bg-zinc-900 w-full text-white z-50">
-      <Nav.List className="flex p-3 gap-3">
+    <Nav.Root className="fixed top-0 md:bg-zinc-900 md:w-full text-white z-50">
+      <button
+        className="md:hidden absolute p-5 z-50 bg-zinc-900 bg-opacity-60 rounded-full"
+        onClick={() => setIsMenuOpen(prev => !prev)}
+      >
+        <SlOptionsVertical className={`${isMenuOpen ? 'rotate-90' : ''} transition-transform`} />
+      </button>
+      <Nav.List
+        ref={menuRef}
+        className={`w-40 h-screen flex flex-col gap-3 px-3 pt-16  
+        ${isMenuOpen ? 'translate-x-0' : '-translate-x-full '} 
+          transition-transform md:flex md:flex-row md:h-fit md:w-full md:-translate-x-0 md:p-3 w-80% bg-zinc-900`}
+      >
         {menuList.map(menu => {
           return <MenuItem key={menu.key} href={menu.href} locationName={menu.locationName} />;
         })}
       </Nav.List>
+      {/* 
+      <Nav.List className="hidden md:flex p-3 gap-3 ">
+        {menuList.map(menu => {
+          return <MenuItem key={menu.key} href={menu.href} locationName={menu.locationName} />;
+        })}
+      </Nav.List> */}
     </Nav.Root>
   );
 }
