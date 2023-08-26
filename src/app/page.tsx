686ff7724ccf7ae3a0ba894cdcc21bffce60f90a
powercording <adoim@naver.com>
2023-08-26 22:40:15 +0900

    Feat: 도커 컴포즈를 통한 도커 올리기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default async function Home({ searchParams }: Home) {
   return (
     <PostGrid
       leftMenu={<FilteredCategory current={searchParams.category} />}
-      rightMenu={<div></div>}
+      rightMenu={<div>right side menu</div>}
     >
       {posts.length ? (
         posts.map(post => {
