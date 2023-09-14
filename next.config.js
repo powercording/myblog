5041ca0723a02b171301738d9a0e6d5c9f48f0ec
powercording <adoim@naver.com>
2023-09-14 18:24:35 +0900

    Fix: 런타임 브라우저 환경변수 제공

--- next.config.js
+++ next.config.js
@@ -14,6 +14,12 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_S3_ENDPOINT: process.env.NEXT_PUBLIC_S3_ENDPOINT,
+    NEXT_PUBLIC_S3_BUCKET: process.env.NEXT_PUBLIC_S3_BUCKET,
+    NEXT_PUBLIC_S3_SECRET_KEY: process.env.NEXT_PUBLIC_S3_SECRET_KEY,
+    NEXT_PUBLIC_S3_KEY: process.env.NEXT_PUBLIC_S3_KEY,
+  },
 };
 
 module.exports = nextConfig;
