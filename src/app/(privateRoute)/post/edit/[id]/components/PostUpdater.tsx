ff5f14e90da4863e365ca76268fe0b698a0d325e
sangdon <adoim@naver.com>
2024-01-26 18:38:37 +0900

    Layout: max height of editor and preview

--- src/app/(privateRoute)/post/edit/[id]/components/PostUpdater.tsx
+++ src/app/(privateRoute)/post/edit/[id]/components/PostUpdater.tsx
@@ -1,17 +1,17 @@
 'use client';
 
 import React from 'react';
-import Editor from '@/components/markdown/Editor'; 
-import Preview from '@/components/markdown/Preview'; 
+import Editor from '@/components/markdown/Editor';
+import Preview from '@/components/markdown/Preview';
 import { UpdateMarkdown, deletePost, updatePost } from '@/service/postService';
 import { useToast } from '@/context/useToast';
 
-type PostUpdaterProps = { 
+type PostUpdaterProps = {
   post: UpdateMarkdown;
-}
+};
 
-export default function PostUpdater({ post } : PostUpdaterProps) {
-  const {content, title, ...rest} = post;
+export default function PostUpdater({ post }: PostUpdaterProps) {
+  const { content, title, ...rest } = post;
   const [doc, setDoc] = React.useState<string>(content);
   const toast = useToast();
 
@@ -19,8 +19,9 @@ export default function PostUpdater({ post } : PostUpdaterProps) {
     setDoc(doc);
   }, []);
 
-   const handleMarkdownUpdate = async () => {
-    const updateResult = await updatePost({ ...post, content, title });
+  const handleMarkdownUpdate = async () => {
+    const newTitle = document.querySelector('[data-id="code-submit-title"]').innerHTML;
+    const updateResult = await updatePost({ ...post, content: doc, title: newTitle });
 
     if (!updateResult.ok || updateResult.status !== 200) {
       return toast.addToast(updateResult.error?.message || '다시 시도해 주세요.');
@@ -31,7 +32,7 @@ export default function PostUpdater({ post } : PostUpdaterProps) {
     }
   };
 
-  const handleMarkdownDelete = async () => { 
+  const handleMarkdownDelete = async () => {
     const isDelete = confirm('정말 삭제하시겠습니까?');
     if (!isDelete) return;
 
@@ -43,15 +44,38 @@ export default function PostUpdater({ post } : PostUpdaterProps) {
     if (deleteResult.ok) {
       window.location.href = '/';
     }
-  }
-
+  };
 
   return (
-    <main className="h-screen pt-16">
-      <div className="max-w-screen flex h-full">
+    <main className="h-auto pt-16">
+      <h1
+        id="code-submit-title"
+        data-id="code-submit-title"
+        autoFocus
+        contentEditable
+        data-placeholder="Please enter a title"
+        className="h-auto max-w-full cursor-text px-4 text-3xl before:empty:text-3xl before:empty:text-gray-500 before:empty:content-[attr(data-placeholder)] focus:outline-none"
+      >
+        {title}
+      </h1>
+      <div className="max-w-screen flex min-h-screen">
         <Editor onChange={handleChange} initialDoc={doc} />
-        <Preview doc={doc} />
+        <Preview doc={doc} editable/>
       </div>
+      <section className="flex justify-end">
+        <button
+          onClick={handleMarkdownUpdate}
+          className="m-3 rounded-sm border border-green-300 border-opacity-50 px-3 py-2 text-gray-500 hover:border-opacity-100 hover:text-white"
+        >
+          수정 하기
+        </button>
+        <button
+          onClick={handleMarkdownDelete}
+          className="m-3 rounded-sm border border-green-300 border-opacity-50 px-3 py-2 text-gray-500 hover:border-opacity-100 hover:text-white"
+        >
+          삭제 하기
+        </button>
+      </section>
     </main>
   );
 }
